14c043d3056ec850f2ceb730899cec652a732909
rafaello129 <rr91175342@gmail.com>
2025-11-23 09:24:52 -0500

    Move api/index.ts to src/api and fix import path
    Renamed api/index.ts to src/api/index.ts and updated the import path for app to reflect the new file location.

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import { Request, ParamsDictionary, Response } from 'express-serve-static-core';
 import { IncomingMessage, ServerResponse } from 'http';
 import { ParsedQs } from 'qs';
-import app from '../src/app';
+import app from '../app';
 
 export default function handler(req: IncomingMessage | Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>, number> | ServerResponse<IncomingMessage>) {
   app(req, res);
